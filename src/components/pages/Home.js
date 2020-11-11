b2076a0dc7cf622d1c749ce38b1952288bdc2c23
davjonz <davjonz@yahoo.com>
2020-11-11 07:30:57 -0500

    Deploy site

---
+++ src/components/pages/Home.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+import Users from '../users/Users';
+import Search from '../users/Search';
+
+
+export const Home = () => {
+  return (
+    <Fragment>
+      <Search />
+      <Users />
+    </Fragment>
+  )
+}
+
+export default Home;
